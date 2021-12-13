6c974223916c5bda9e3b1da8f0657b71aece8497
CJJ <caijingjing2001@163.com>
2021-12-12 17:43:32 -0800

    ejs basicly successful

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const ejs = require("ejs")
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://172.21.2.236:27017/190110910802');
 
@@ -15,10 +16,13 @@ const Cat = mongoose.model('cat1s', schema);
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
-    res.send(req.query)
+    // res.send(req.query)
     console.log(req.query)
     const kitty = new Cat({ name: req.query.first,health : req.query.second });
     kitty.save()
+    ejs.renderFile("result.html",{returnValue:"success"},(err,str)=>{
+        res.send(str)
+    })
     // res.send(req.body)
     // console.log(req.body)
     // res.end()
